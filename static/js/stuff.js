0d8004f7560d67e94a433b29b70c5b40dbbc521a
Caroline Davidson <caroline.davidson@comcast.net>
2014-02-19 19:10:44 -0800

    Completed Hello World problem!

---
+++ static/js/stuff.js
@@ -0,0 +1,23 @@
+alert('Hello and welcome to this page of items!'); // edit me!
+
+// Problem 1 (Say Hello!) ---------------------------------------------------
+$('#say_hello').click(function() {alert('Hello world!')
+});
+
+
+// Problem 2 (Houdini) ------------------------------------------------------
+$('#disappear').click(function() {
+  //WRITE CODE HERE
+});
+
+$('#reappear').click(function() {
+  //WRITE CODE HERE
+});
+
+
+// Problem 3 (Tickle Me Pink) -----------------------------------------------
+// WRITE CODE HERE
+
+
+// Problem 4 (Greet Me) -----------------------------------------------------
+// WRITE CODE HERE
