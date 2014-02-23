8f610ffbad21cdcb23854487541ff2560310ec96
Caroline Davidson <caroline.davidson@comcast.net>
2014-02-22 22:32:38 -0800

    Added the bit about cats, but cannot make them disappear for some reason

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -21,8 +21,22 @@ $('#not_pink').click(function() {
  alert('Hey, love, it will be alright')
 });
 
-// Problem 4 (Greet Me) -----------------------------------------------------
 $('#namey').click(function() {
 $('#my_name').val()
-alert('Hello ' + $('#my_name').val())
+alert('Hello ' + $('#my_name').val() + '!')
 });
+
+function catty() {
+        var src = "https://googledrive.com/host/0BwLGvM-eya6sc3BHWkZQVEVscFU/catz.png";
+        show_image("https://googledrive.com/host/0BwLGvM-eya6sc3BHWkZQVEVscFU/catz.png", 623,791, "Catz");
+    }
+
+
+    function show_image(src, width, height, alt) {
+        var img = document.createElement("img");
+        img.src = src;
+        img.width = width;
+        img.height = height;
+        img.alt = alt;
+        document.body.appendChild(img);
+    }
