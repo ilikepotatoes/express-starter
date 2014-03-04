4af3fa4c5adb1eaa25c3030e20fe48dd36c927dc
Caroline Davidson <caroline.davidson@comcast.net>
2014-03-04 15:17:44 -0800

    Almost done

---
+++ static/js/imgrr.js
@@ -0,0 +1,20 @@
+$('#toggle_img').click(function() {
+   if($('#toggle_img').text()==='Go Away!'){
+		$('#toggle_img').text('Come Back!')
+		$('#animalz').hide()
+} else {$('#toggle_img').text('Go Away!')
+		$('#animalz').show()
+	}});
+
+$('#change_img').click(function(){
+	alert('/static/img/' + $('#new_img_file').val())
+		$('#animalz').attr('src', '/static/img/' + $('#new_img_file').val())
+});
+
+// Problem 4 (Find the Source) -------------------------------------------------
+$('.clickable').click(function() {
+alert($(this).attr('src'))
+});
+
+// Problem 5 (Imgrr) -------------------------------------------------
+// WRITE CODE HERE
