bd9aff8790f605281448129d648abc780ed0f410
Caroline Davidson <caroline.davidson@comcast.net>
2014-03-11 14:49:00 -0700

    Look at all the colors!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -8,20 +8,23 @@ $(document).ready(function() {
   });
 
 //-----------------------------------------------------------
-// p1 - yay!
+// p1 - rectangle
   $('#p1').click(function() {
+    context.strokeStyle='000000'
     context.strokeRect(150, 175, 200, 100);
   });
 
 //-----------------------------------------------------------
-// p2 - yay!
+// p2 - square
   $('#p2').click(function() {
+    context.strokeStyle='000000'
     context.strokeRect(150, 75, 100, 100);
   });
 
 //-----------------------------------------------------------
-// p3
+// p3 - arc
   $('#p3').click(function() {
+  	context.strokeStyle='000000'
   	context.beginPath();
     context.arc(250, 75, 50, 4, 1);
     context.closePath();
@@ -29,8 +32,9 @@ $(document).ready(function() {
   });
 
 //-----------------------------------------------------------
-// p4
+// p4 - circle
   $('#p4').click(function() {
+  	context.strokeStyle='000000'
   	context.beginPath();
     context.arc(300, 175, 50, 0, 6);
     context.closePath();
@@ -38,13 +42,44 @@ $(document).ready(function() {
   });
 
 //-----------------------------------------------------------
-// p5
+// p5 - line
   $('#p5').click(function() {
+  	context.strokeStyle='000000'
   	context.beginPath();
     context.moveTo(100, 70);
     context.lineTo(255, 200)
     context.closePath();
     context.stroke();
+  });
+
+ //-----------------------------------------------------------
+// p6 - green rectangle
+  $('#p6').click(function() {
+    context.strokeStyle='4DDB4D'
+    context.strokeRect(150, 175, 200, 100);
+    context.fillStyle='009900'
+    context.fillRect(150, 175, 200, 100);
+  });
+
+//-----------------------------------------------------------
+// p7 - red circle
+  $('#p7').click(function() {
+  	context.strokeStyle='E60000'
+  	context.beginPath();
+    context.arc(250, 325, 50, 0, 6);
+    context.closePath();
+    context.stroke()
+    context.fillStyle='C63333'
+    context.fill();
+  });
+
+//-----------------------------------------------------------
+// p8 - yellow square, blue outline
+  $('#p8').click(function() {
+    context.strokeStyle='0099FF'
+    context.strokeRect(250, 310, 100, 100)
+    context.fillStyle='FFFF80'
+    context.fillRect(250, 310, 100, 100);
   });
   //---------------------------------------------------------------------------
   //Write your code for p1-p12 here
