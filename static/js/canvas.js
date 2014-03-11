d971e1fcd223009e44921dedd73e24d1b249b564
Caroline Davidson <caroline.davidson@comcast.net>
2014-03-11 14:19:26 -0700

    Finished with shapes!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -18,6 +18,34 @@ $(document).ready(function() {
   $('#p2').click(function() {
     context.strokeRect(150, 75, 100, 100);
   });
+
+//-----------------------------------------------------------
+// p3
+  $('#p3').click(function() {
+  	context.beginPath();
+    context.arc(250, 75, 50, 4, 1);
+    context.closePath();
+    context.stroke();
+  });
+
+//-----------------------------------------------------------
+// p4
+  $('#p4').click(function() {
+  	context.beginPath();
+    context.arc(300, 175, 50, 0, 6);
+    context.closePath();
+    context.stroke();
+  });
+
+//-----------------------------------------------------------
+// p5
+  $('#p5').click(function() {
+  	context.beginPath();
+    context.moveTo(100, 70);
+    context.lineTo(255, 200)
+    context.closePath();
+    context.stroke();
+  });
   //---------------------------------------------------------------------------
   //Write your code for p1-p12 here
   //
